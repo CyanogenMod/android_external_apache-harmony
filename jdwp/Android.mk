320d001e98a7a0aa26e1b0be90f410b11c21413c
Sebastien Hertz <shertz@google.com>
2016-02-22 11:06:14 +0100

    JDWP: compile tests for 1.8
    Bug: 27262745
    Bug: 27218415
    
    (cherry picked from commit b736af0badb79b89f59c6af6872688b8fd131941)
    
    Change-Id: I7e479109a9c71abc502b4cc470e1dbd45af64139

--- jdwp/Android.mk
+++ jdwp/Android.mk
@@ -52,6 +52,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(harmony_jdwp_test_src_files)
 LOCAL_JAVA_LIBRARIES := junit
 LOCAL_MODULE := apache-harmony-jdwp-tests-host
+LOCAL_JAVA_LANGUAGE_VERSION := 1.8
 include $(BUILD_HOST_JAVA_LIBRARY)
 
 ifeq ($(HOST_OS),linux)
@@ -59,6 +60,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(harmony_jdwp_test_src_files)
 LOCAL_JAVA_LIBRARIES := junit-hostdex
 LOCAL_MODULE := apache-harmony-jdwp-tests-hostdex
+LOCAL_JAVA_LANGUAGE_VERSION := 1.8
 include $(BUILD_HOST_DALVIK_JAVA_LIBRARY)
 endif  # HOST_OS == linux
 
